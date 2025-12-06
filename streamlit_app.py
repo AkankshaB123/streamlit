9035c1ef864a1026ea8664fcc45855d67623babf
Akanksha Bakshi <akankshabakshi123@gmail.com>
2025-12-06 19:34:12 +0530

    Remove pip install streamlit from streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,8 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/1nTsOtwBNrN7yw0pDzG1ZV-A0naPGLZps
 """
 
-pip install streamlit
-
 import streamlit as st
 
 print("Hello Streamlit!")
